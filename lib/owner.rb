9f8e7db90d0a63217ddaefaf4eb4996ab65bd4b6
Danniely Santos <dannielyingrid@gmail.com>
2020-09-12 04:07:13 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,52 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  attr_reader :cats
+  attr_reader :dogs
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @cats = []
+    @dogs = []
+    @@all << self
+  end
+  
+  def buy_cat(cat)
+    @cats << Cat.new(cat, self)
+  end
+    
+  def buy_dog(dog)
+    @dogs << Dog.new(dog, self)
+  end
+  
+  def walk_dogs
+    @dogs.each do |dog|
+      dog.mood = "happy"
+    end
+  end
+  
+  def feed_cats
+    @cats.each do |cat|
+      cat.mood = "happy"
+    end
+  end
+  
+  def say_species
+    "I am a #{species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    all.count
+  end
+  
+  def self.reset_all
+    all.clear()
+  end
+
 end
