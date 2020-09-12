b343951f9ffa1323292bc5a1e47ad444477bfcfc
Danniely Santos <dannielyingrid@gmail.com>
2020-09-12 17:46:33 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -22,15 +22,39 @@ class Owner
   end
   
   def walk_dogs
-    @dogs.each do |dog|
+    dogs.each do |dog|
       dog.mood = "happy"
     end
   end
   
   def feed_cats
-    @cats.each do |cat|
+    cats.each do |cat|
       cat.mood = "happy"
     end
+  end
+  
+  def sell_pets
+    cats.each do |cat|
+      cat.mood = "nervous"
+      cat.owner = nil
+    end
+    
+    dogs.each do |dog|
+      dog.mood = "nervous"
+      dog.owner = nil
+    end
+  end
+  
+  def cats
+    Cat.all.select { |cat| cat.owner == self }
+  end
+  
+  def dogs
+    Dog.all.select { |dog| dog.owner == self }
+  end
+  
+  def list_pets
+    "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
   end
   
   def say_species
